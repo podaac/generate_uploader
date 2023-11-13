c251bec475a67955070e28c86fdca1f3c6dba159
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2023-11-13 20:31:09 +0000

    Set cumulus SNS topic to throttled for sit and uat

--- uploader/Uploader.py
+++ uploader/Uploader.py
@@ -105,7 +105,10 @@ class Uploader:
         self.processing_type = processing_type
         self.dataset = dataset
         self.logger = logger
-        self.cumulus_topic = f"podaac-{venue}-cumulus-provider-input-sns"
+        if venue == "ops":
+            self.cumulus_topic = f"podaac-{venue}-cumulus-provider-input-sns"
+        else:
+            self.cumulus_topic = f"podaac-{venue}-cumulus-throttled-provider-input-sns"
         self.cross_account = self.get_cross_account_id(prefix)
         self.processed = []
         self.provenance = []
